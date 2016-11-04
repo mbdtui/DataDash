195a56bb68d003c869a1b3f572c0fdde539acdbc
Young Lee <leeyoungkyun77@gmail.com>
2016-11-04 17:10:09 -0400

    MailUtil
    This allows user to send mail by calling sendMail function.
    I used nodemailer library

---
+++ mailUtil.js
@@ -0,0 +1,41 @@
+var nodemailer = require('nodemailer');
+var poolConfig = {
+  pool: true,
+  service: 'gmail',
+  secure: true, 
+  auth: {
+    user: 'dmubmdmtyui@gmail.com',
+    pass: 'MyPetName'
+  }
+};
+
+// create reusable transporter object using the default SMTP transport
+var transporter = nodemailer.createTransport(poolConfig);
+
+// verify connection configuration
+transporter.verify(function(error, success) {
+  if (error) {
+    console.log("Invalid SMTP configuration");
+    console.log(error);
+    process.exit(1);
+  } 
+});
+
+
+// setup e-mail data with unicode symbols
+var mailOptions = {
+  from: '"dmubmdmtyui@gmail.com" <dmubmdmtyui@gmail.com>', // sender address
+  subject: 'Do not reply', // Subject line
+};
+
+exports.sendMail = function(toNames, html){
+  mailOptions.to = toNames;
+  mailOptions.html = html;
+  transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+      return console.log(error);
+    }
+    console.log('Message sent: ' + info.response);
+  });
+}
+
