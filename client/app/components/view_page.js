df97113fe3fbaeb4d1018a59217d9c8c791aa82e
Justin Martinelli <justinmartinelli1@gmail.com>
2016-12-04 17:16:33 -0500

    change class names

--- client/app/components/view_page.js
+++ client/app/components/view_page.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router';
 
-export default class Login extends React.Component{
+export default class View extends React.Component{
 
   render(){
     return(
