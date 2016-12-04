47fb802f50a70a0548fcb381f825a6bad312f268
Makenzie Schwartz <schwartz.makenzie@gmail.com>
2016-12-04 15:43:14 -0500

    updated package files

--- client/app.js
+++ client/app.js
@@ -5,6 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var mongoose = require('mongoose');
+
+// MongoDB connection
+mongoose.connect('mongodb://localhost:27017/datadash');
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 
