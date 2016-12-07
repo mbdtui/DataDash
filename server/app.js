802124cae586e086ce027d6a6cd3e4b740e46115
Makenzie Schwartz <schwartz.makenzie@gmail.com>
2016-12-07 13:44:09 -0500

    removed server app.js files

--- server/app.js
+++
@@ -1,9 +0,0 @@
-var mongo = require('./mongoAccessor.js');
-mongo.createJournalEntry("14", "name", "group", "hello", "review", [], false);
-mongo.readJournalEntries(function(items){
-  console.log(items);
-  mongo.deleteJournalEntry({ macroID : "14" });
-  mongo.readJournalEntries(function(items){
-    console.log(items);
-  });
-});
