adbeb3b74af68deff2aa607e27bf83423929a28e
Thien Dinh <tdinhcs@gmail.com>
2016-12-11 14:20:47 -0500

    Add routes to receive available functions for Delete and Update tables.

--- server/server.js
+++ server/server.js
@@ -24,6 +24,66 @@ app.get('/macro/:macroid', function(req, res){
     res.send(getMacroData(req.params.macroid));
 });
 
+app.get('/macros_for_table/:table_name/update', function(req, res) {
+	var table_name = req.params.table_name;
+	var available_macros;
+	switch(table_name) {
+		case 'driver_step':
+			available_macros = [
+			'update_active_step_indicator_by_driverstepid',
+			'update_active_step_indicator_by_runname_driverstepid',
+			'update_active_step_indicator_by_runname',
+			'update_active_step_indicator_by_runname_groupnumber'
+			];
+			break;
+		case 'driver_schedule':
+			available_macros = [
+			'update_schedule_starttime_by_runname_auditid',
+			'update_status_code_by_runname_auditid',
+			'update_valuation_enddate_by_runname_auditid',
+			'update_valuation_startdate_by_runname_auditid',
+			'update_sla_date_time_by_auditid',
+			'update_sla_date_time_by_runname',
+			'update_historical_sla_date_time_by_runname'
+			];
+			break;
+		case 'driver_step_detail':
+			available_macros = [
+			'update_run_status_code_by_runname_groupnumber',
+			'update_run_status_code_by_runname_driverstepdetail_id'
+			];
+			break;
+	}
+	// console.log(available_macros);
+	res.send({ macros: available_macros});
+});
+
+app.get('/macros_for_table/:table_name/delete', function(req, res) {
+	var table_name = req.params.table_name;
+	var available_macros;
+	switch(table_name) {
+		case 'driver_step':
+			available_macros = [
+			'delete_all_entries_by_runname',
+			'delete_all_entries_by_runname_groupnumber',
+			'delete_all_entries_by_runname_driverstepid'
+			];
+			break;
+		case 'driver_schedule':
+			available_macros = [
+			'delete_all_entries_by_runname'
+			];
+			break;
+		case 'driver_step_detail':
+			available_macros = [
+			'delete_all_entries_by_runname'
+			];
+			break;
+	}
+	// console.log(available_macros);
+	res.send({ macros: available_macros});
+});
+
 app.post('/view_run_status_code', function(req, res) {
 	var app_name = req.body.app_name;
     var run_name = req.body.run_name;
@@ -38,6 +98,8 @@ app.post('/view_run_status_code', function(req, res) {
 		}
 	});
 });
+
+
 
 function getMacroData(macroIDs){
     //macroIDs=comma separated list of ids
