5f64a5be1cfa83b7167f3e67f3eedd325c3b0932
michaelmarcello <michaelmarcello@Michaels-MacBook-Pro.local>
2016-12-13 21:52:54 -0500

    Added functionality to Mail API to make the email cleaner and clearer.

--- server/mailUtil.js
+++ server/mailUtil.js
@@ -39,19 +39,80 @@ exports.sendMail = function(toNames, jsonObject, callback){
   switch(jsonObject.table){
     case 'c_driver_schedule':
       if(jsonObject.function_called == 'update_schedule_starttime_by_runname_auditid'){
-        paramString = "Update schedule start time by run name and auditID.  MacroID: " + jsonObject.macroID + " with the following parameters: run name: " +
+        paramString = "c_driver_schedule: Update schedule start time by run name and auditID.  MacroID: " + jsonObject.macroID + " with the following parameters: run name: " +
         jsonObject.params.run_name + "  auditID: " + jsonObject.params.audit_id + "  schedule start time: " + jsonObject.params.schedule_start_time;
       }
       if(jsonObject.function_called == 'update_status_code_by_runname_auditid'){
-        paramString = "Update status code by run name and auditID.  MacroID: " + jsonObject.macroID + " with the following parameters: run name: " +
+        paramString = "c_driver_schedule: Update status code by run name and auditID.  MacroID: " + jsonObject.macroID + " with the following parameters: run name: " +
         jsonObject.params.run_name + " auditID: " + jsonObject.params.audit_id + " status code: " + jsonObject.params.status_code);
       }
       if(jsonObject.function_called == 'update_valuation_enddate_by_runname_auditid'){
-        paramString = "Update status code by run name and auditID.  MacroID: " + jsonObject.macroID + " with the following parameters: run name: " +
-        jsonObject.params.run_name + " auditID: " + jsonObject.params.status_code + " valuation end date:" + jsonObject.valuation_end_date);
+        paramString = "c_driver_schedule: Update status code by run name and auditID.  MacroID: " + jsonObject.macroID + " with the following parameters: run name: " +
+        jsonObject.params.run_name + " auditID: " + jsonObject.params.status_code + " valuation end date:" + jsonObject.params.valuation_end_date);
       }
-      //if(jsonObject.function_called == 'update_')
+      if(jsonObject.function_called == 'update_valuation_startdate_by_runname_auditid'){
+        paramString = "c_driver_schedule: Update valuation start date by run name and auditID.  MacroID: " + jsonObject.macroID + " with the following parameters: run name: " +
+        jsonObject.params.run_name + " auditID: " + jsonObject.params.status_code + " valuation start date:" + jsonObject.params.valuation_end_date);
+      }
+      if(jsonObject.function_called == 'update_sla_date_time_by_auditid'){
+        paramString = "c_driver_schedule: Update sla date and time by auditID.  MacroID: " + jsonObject.macroID + " with the following parameters: auditID: " +
+        jsonObject.params.audit_id + " date: " + jsonObject.params.date + " time:" + jsonObject.params.valuation_end_date);
+      }//audit_id, date, time
+      if(jsonObject.function_called == 'update_sla_date_time_by_runname'){
+        paramString = "c_driver_schedule: Update sla date and time by run name.  MacroID: " + jsonObject.macroID + " with the following parameters: run name: " +
+        jsonObject.params.run_name + " date: " + jsonObject.params.date + " time:" + jsonObject.params.time);
+      }//run_name, date, time
+      if(jsonObject.function_called == 'update_historical_sla_date_time_by_runname'){
+        paramString = "c_driver_schedule: Update historical sla date and time by run name.  MacroID: " + jsonObject.macroID + " with the following parameters: run name: " +
+        jsonObject.params.run_name + " date: " + jsonObject.params.date + " time:" + jsonObject.params.time);
+      }//run_name, date, time
+      if(jsonObject.function_called == 'delete_all_entries_by_runname'){
+        paramString = "c_driver_schedule: Delete all entries by runname.  MacroID: " + jsonObject.macroID + " with the following parameters: run name: " + jsonObject.params.run_name);
+      }
+      break;
 
+    case 'c_driver_step':
+      if(jsonObject.function_called == 'delete_all_entries_by_runname'){
+        paramString = "c_driver_step: Delete all entries by runname.  MacroID: " + jsonObject.macroID + " with the following parameters: run name: " + jsonObject.params.run_name);
+      }
+      if(jsonObject.function_called == 'delete_all_entries_by_runname_groupnumber'){
+        paramString = "c_driver_step: Delete all entries by runname and group number.  MacroID: " + jsonObject.macroID + " with the following parameters: run name: " + jsonObject.params.run_name +
+      " group number: " + jsonObject.params.group_number);
+      }//run_name, group_number
+      if(jsonObject.function_called == 'delete_all_entries_by_runname_driverstepid'){
+        paramString = "c_driver_step: Delete all entries by runname and driver step id.  MacroID: " + jsonObject.macroID + " with the following parameters: run name: " + jsonObject.params.run_name +
+      " driver step ID: " + jsonObject.params.driver_step_id);
+      }//run_name, driver_step_id
+      if(jsonObject.function_called == 'update_active_step_indicator_by_driverstepid'){
+        paramString = "c_driver_step: Update active step indicator by driver step ID.  MacroID: " + jsonObject.macroID + " with the following parameters: driver step ID: " +
+        jsonObject.params.driver_step_id + " active_step_indicator: " + jsonObject.params.active_step_indicator);
+      }//driver_step_id, active_step_indicator
+      if(jsonObject.function_called == 'update_active_step_indicator_by_runname_driverstepid'){
+        paramString = "c_driver_step: Update active step indicator by run name and driver step ID.  MacroID: " + jsonObject.macroID + " with the following parameters: run name: " +
+        jsonObject.params.run_name + " driver step ID: " + jsonObject.params.driver_step_id + " active step indicator:" + jsonObject.params.active_step_indicator);
+        }//run_name, driver_step_id, active_step_indicator
+      if(jsonObject.function_called == 'update_active_step_indicator_by_runname'){
+        paramString = "c_driver_step: Update active step indicator by run name.  MacroID: " + jsonObject.macroID + " with the following parameters: run name: " +
+        jsonObject.params.run_name + " active step indicator:" + jsonObject.params.active_step_indicator);
+      }//run_name, active_step_indicator
+      if(jsonObject.function_called == 'update_active_step_indicator_by_runname_groupnumber'){
+        paramString = "c_driver_step: Update active step indicator by run name and group number.  MacroID: " + jsonObject.macroID + " with the following parameters: run name: " +
+        jsonObject.params.run_name + " group number: " + jsonObject.params.group_number + " active step indicator:" + jsonObject.params.active_step_indicator);
+      }//run_name, group_number, active_step_indicator
+      break;
+
+    case 'c_driver_step_detail':
+    if(jsonObject.function_called == 'delete_all_entries_by_runname'){
+      paramString = "c_driver_step_detail: Delete all entries by runname.  MacroID: " + jsonObject.macroID + " with the following parameters: run name: " + jsonObject.params.run_name);
+    }//run_name
+    if(jsonObject.function_called == 'update_run_status_code_by_runname_groupnumber'){
+      paramString = "c_driver_step_detail: Update run status code by run name and group number.  MacroID: " + jsonObject.macroID + " with the following parameters: run name: " +
+      jsonObject.params.run_name + " group number: " + jsonObject.params.group_number + " run status code:" + jsonObject.params.run_status_code);
+    }//run_name, group_number, run_status_code
+    if(jsonObject.function_called == 'update_run_status_code_by_runname_driverstepdetail_id'){
+      paramString = "c_driver_step_detail: Update run status code by run name and driver step detail ID.  MacroID: " + jsonObject.macroID + " with the following parameters: run name: " +
+      jsonObject.params.run_name + " driver step detail id: " + jsonObject.params.driver_step_detail_id + " run status code:" + jsonObject.params.run_status_code);
+    }//run_name, driver_step_detail_id, run_status_code
 
   }
 
